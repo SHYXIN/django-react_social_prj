20d20ec8b249e1d45e6922a805b735a63864df28
Py_TaiYuan_WangXin <wangxin@zhulong.com.cn>
2024-08-26 09:41:03 +0800

    第一次提交

---
+++ social-media-app/src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+// import { randomAvatar } from "../utils";
+import { Navbar, Container, Image, NavDropdown, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { getUser, useUserActions } from "../hooks/user.actions";
+
+
+function Navigationbar() {
+  const userActions = useUserActions()
+
+  const user = getUser();
+
+  return (
+    <Navbar bg="primary" variant="dark">
+      <Container>
+        <Navbar.Brand className="fw-bold" href="#home">
+          Postagram
+        </Navbar.Brand>
+        <Navbar.Collapse className="justify-content-end">
+          <Nav>
+            <NavDropdown
+              title={
+                <Image
+                  // src={randomAvatar()}
+                  src={user.avatar}
+                  roundedCircle
+                  width={36}
+                  height={36}
+                />
+              }
+            >
+              <NavDropdown.Item as={Link} to={`/profile/${user.id}/`}>Profile</NavDropdown.Item>
+              <NavDropdown.Item onClick={userActions.logout}>Logout</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navigationbar;
